b91a82083e2b8d210f24a90c0e38ec206adb7b71
rlatjsrnr <ksk4457@gmail.com>
2023-11-12 22:42:54 +0900

    firstcommit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
@@ -20,6 +21,7 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*
             Member member = new Member();
             member.setId(1L);
             member.setName("hello");
@@ -46,6 +48,49 @@ public class JpaMain {
             for(Member m : result){
                 System.out.println("Member.name = " + m.getName());
             }
+
+            Member findMember1 = em.find(Member.class, 2L);
+            Member findMember2 = em.find(Member.class, 2L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
+
+
+            // insert 문을 영속성 컨택스트 내에 저장 해두고 커밋이 되면 한 번에 모든 insert 를 수행
+
+            Member member3 = new Member(150L, "A");
+            Member member4 = new Member(160L, "B");
+
+            em.persist(member3);
+            em.persist(member4);
+
+
+            Member member5 = new Member(170L, "C");
+            em.persist(member5);
+            member.setName("ZZZZZ");
+
+            Member member6 = new Member(200L, "member6");
+            em.persist(member6);
+            em.flush();
+
+            Member mem = em.find(Member.class, 200L);
+            mem.setName("AAAAA");
+            // 특정 entity만 준 영속 상태로 전환
+            em.detach(mem);
+            // 영속성 컨택스트 초기화
+            em.clear();
+
+            System.out.println("==========================");
+            mem = em.find(Member.class, 200L);
+
+             */
+
+            Member member = new Member();
+            member.setId(2L);
+            member.setUsername("user1");
+            member.setRoleType(RoleType.ADMIN);
+            em.persist(member);
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -53,5 +98,7 @@ public class JpaMain {
             em.close();
         }
         emf.close();
+
+
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,29 +1,99 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+    @Column(name="name", nullable = false)
+    private String username;
+    private int age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+    @Lob
+    private String description;
+    @Transient
+    private int temp;
+
+    public Member() {
+    }
+
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createDate, Date lastModifiedDate, String description) {
+        this.id = id;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createDate = createDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
